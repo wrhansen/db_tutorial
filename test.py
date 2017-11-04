a8861234188188a1c6ee1d2f869dfe253403eddb
Wesley Hansen <wesrhansen@gmail.com>
2017-11-03 23:21:18 -0400

    python3.6 f-strings!

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3.6
 import os
 from subprocess import Popen, PIPE
 import unittest
@@ -5,7 +6,8 @@ import unittest
 
 class DBTestCase(unittest.TestCase):
     def run_command(self, input_lines):
-        stdin = '{}\n'.format('\n'.join(input_lines))
+        input_line_str = '\n'.join(input_lines)
+        stdin = f'{input_line_str}\n'
         with Popen(
             ['./db', 'mydb.db'],
                 stdout=PIPE,
@@ -37,7 +39,7 @@ class DBTestCase(unittest.TestCase):
         Prints an error message when table is full (after 1400 rows)
         '''
         fill_stdin = [
-            'insert {0} user{0} person{0}@example.com'.format(index)
+            f'insert {index} user{index} person{index}@example.com'
             for index in range(1, 1402)
         ] + ['.exit']
         self.assertEqual(
@@ -46,23 +48,17 @@ class DBTestCase(unittest.TestCase):
     def test_allows_inserting_strings_of_maximum_length(self):
         long_username = 'a' * 32
         long_email = 'a' * 255
-        script = [
-            'insert 1 {} {}'.format(long_username, long_email), 'select',
-            '.exit'
-        ]
+        script = [f'insert 1 {long_username} {long_email}', 'select', '.exit']
         self.assertEqual(
             self.run_command(script), [
-                'db > Executed.', 'db > (1, {}, {})'.format(
-                    long_username, long_email), 'Executed.', 'db > '
+                'db > Executed.', f'db > (1, {long_username}, {long_email})',
+                'Executed.', 'db > '
             ])
 
     def test_prints_error_message_if_strings_too_long(self):
         long_username = 'a' * 33
         long_email = 'a' * 256
-        script = [
-            'insert 1 {} {}'.format(long_username, long_email), 'select',
-            '.exit'
-        ]
+        script = [f'insert 1 {long_username} {long_email}', 'select', '.exit']
         self.assertEqual(
             self.run_command(script),
             ['db > String is too long.', 'db > Executed.', 'db > '])
